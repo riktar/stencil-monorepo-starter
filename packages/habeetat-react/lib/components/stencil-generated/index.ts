35b5a0e2d335f3be7580b6eb5b2651cdecbc72f5
Riccardo Tartaglia <tartaglia.riccardo@gmail.com>
2023-11-28 17:34:45 +0100

    initial

---
+++ packages/habeetat-react/lib/components/stencil-generated/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'habeetat-components';
+
+
+
+export const HabButton = /*@__PURE__*/createReactComponent<JSX.HabButton, HTMLHabButtonElement>('hab-button');
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
